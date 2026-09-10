8c2524b028d8a73deb442fa063a72668c385c706
k0burt <kobymarcroft@rhs.school.nz>
2026-09-10 12:05:32 +1200

    deliverable 2
    the buttons have now been placed in a grid, keeping the same functionality as before.

--- as91901.py
+++ as91901.py
@@ -8,31 +8,38 @@ class WhackAMole(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setGeometry(0, 0, 300, 300)
-
         # Create variables
         self.score = 0
 
         # Create labels
         self.lbl_score = QLabel(f"Score: {self.score}")
 
-        # Create button
-        self.mole = QPushButton(self)
-        self.mole.setText("MOLE")
-        self.mole.setStyleSheet('width: 50px; height: 50px; background-color: brown; border-radius: 50%')
-        self.mole.clicked.connect(self.mole_clicked)
+        # Create grid
+        self.mole_layout = QGridLayout()
+        self.mole_buttons = []
+        for row in range(4):
+            mole_rows = []
+            for col in range(4):
+                self.mole = QPushButton(self)
+                self.mole.setFixedSize(100, 100)
+                self.mole.setStyleSheet('background-color: brown')
+                self.mole.setText("MOLE")
+                self.mole_layout.addWidget(self.mole, row, col)
+                mole_rows.append(self.mole)
+                self.mole.clicked.connect(lambda checked, r=row, c=col: self.mole_clicked(r, c))
+            self.mole_buttons.append(mole_rows)
 
         # Create layouts
         self.v_layout =  QVBoxLayout()
         self.v_layout.addWidget(self.lbl_score)
-        self.v_layout.addWidget(self.mole)
+        self.v_layout.addLayout(self.mole_layout)
 
         # Display everything
         self.main = QWidget()
         self.main.setLayout(self.v_layout)
         self.setCentralWidget(self.main)
 
-    def mole_clicked(self):
+    def mole_clicked(self, row, col):
         self.score += 1
         self.lbl_score.setText(f"Score: {self.score}")
 
