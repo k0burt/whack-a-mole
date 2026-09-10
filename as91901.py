bdcf988c40653067ef527a82d1aa004f31ff5d7e
k0burt <kobymarcroft@rhs.school.nz>
2026-09-10 12:24:51 +1200

    deliverable 3
    there is now a random mole selected in the grid and only that mole can be clicked to increase the score

--- as91901.py
+++ as91901.py
@@ -22,7 +22,8 @@ class WhackAMole(QMainWindow):
             for col in range(4):
                 self.mole = QPushButton(self)
                 self.mole.setFixedSize(100, 100)
-                self.mole.setStyleSheet('background-color: brown')
+                self.mole.setStyleSheet('background-color: #202020; border-radius: 50%;')
+                self.mole.setEnabled(False)
                 self.mole.setText("MOLE")
                 self.mole_layout.addWidget(self.mole, row, col)
                 mole_rows.append(self.mole)
@@ -39,9 +40,29 @@ class WhackAMole(QMainWindow):
         self.main.setLayout(self.v_layout)
         self.setCentralWidget(self.main)
 
+        self.mole_show()
+
     def mole_clicked(self, row, col):
+        # Update score
         self.score += 1
         self.lbl_score.setText(f"Score: {self.score}")
+
+        # Disable clicked mole
+        self.mole_buttons[row][col].setEnabled(False)
+        self.mole_buttons[row][col].setStyleSheet(f'background-color: #202020; border-radius: 50%;')
+
+        # Show random mole
+        self.mole_show()
+
+    def mole_show(self):
+        # Pick random mole
+        mole_row = randint(0, 3)
+        mole_col = randint(0, 3)
+
+        # Enable random mole
+        self.mole_buttons[mole_row][mole_col].setEnabled(True)
+        self.mole_buttons[mole_row][mole_col].setStyleSheet(f'background-color: #895129; border-radius: 50%;')
+
 
 app = QApplication(sys.argv)
 window = WhackAMole()
